dcac80ac37e06cf8c725089b362d0b972ac2c769
Shridatt Sugrim <ssugrim@winlab.rutgers.edu>
2013-01-05 02:07:27 -0500

    extended plots past original threshold

--- OperatorCurve/OperatorCurve.m
+++ OperatorCurve/OperatorCurve.m
@@ -1,10 +1,5 @@
 % Generates an operator Curve
-
 addpath '../lib'
-
-%Jpeg params
-res = 1600;
-font = 25;
 
 %bounds on alpha effective and beta effective
 alpha = 0.01;
@@ -16,14 +11,14 @@ beta_effective_bound = beta / ( 1 - alpha);
 
 %tuneing parameters
 p_prime = 0.9;
-delta = 0.01;
+delta = 0.09;
 
 %boundaries of accept and reject region
 p_one = p_prime + delta;
 p_zero = p_prime - delta;
 
 %dummy parameter
-h = -500:.01:500;
+h = -500:.0001:500;
 
 %Upper and lower sequential test thresholds (aproximated)
 A = (1 - beta) / alpha;
@@ -47,35 +42,48 @@ h_zero = (log(B)) / (log(p_lower) - log(p_upper));
 h_one = (log(A)) / (log(p_lower) - log(p_upper));
 s = (log(1 / p_upper)) / (log(p_lower) - log(p_upper));
 
-%Graphical test Paramters and Boundaries. 
-m = 0:1:10000;
+%Graphical test Paramters and Boundaries.  Purely for grpahing examples, not used in simulation.
+m = 0:1:1000;
+length(m);
 L_1 = (s .* m) + (h_one .* ones(size(m)));
 L_0 = (s .* m) + (h_zero .* ones(size(m)));
 
-%Graphical Simulation parameters
-p_test = 0.75:.01:1;
+%Simulation paramters
+p_test = 0:.0001:1;
+deadline = 1000000;
+num_steps = 1000;
+
+%Simulation Storage vars
 calc_E_n = zeros(size(p_test));
 calc_p_h1 = zeros(size(p_test));
 
 %for each value of p_test, run simulation to compute the effective p_0 and
-%E[n];
+%E_p[n]
 for j = 1:1:length(p_test);
-    steps = zeros(500,1);
+    
+    %dummy data vars;
+    steps = zeros(num_steps,1);
     decision = zeros(size(steps));
+    
     for i = 1:1:length(steps)
-        cur_samples = count_ones(Sample1dim(p_test(j), length(m)));
-        cur_thres = SeqThreshold(L_1, cur_samples ,L_0);
+        %function that actually generates the data
+        cur_thres = GenOCthreshold(p_test(j),s,h_zero,h_one,deadline)
+        
+        %result analysis / storage
         steps(i)=cur_thres(4);
         if cur_thres(2) > cur_thres(1)
            decision(i)=1;
         end
     end
+    
+    %per p value store the result
     calc_E_n(j)=mean(steps);
     calc_p_h1(j)=mean(decision);
 end
 
+%generic save
 save('./oc_curve.mat');
 
-%plot(p(p > .75), L(p > .75));
+%plot(p(p > .75), L(p > .75),'color','green');
 %hold;
-%plot(p_test,1 - calc_p_h1);
+%plot(p_test,1 - calc_p_h1,'color','red');
